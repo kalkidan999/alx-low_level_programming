3d6fbd7c8538c744cc10897432ae7b1664dc5677
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-04 16:53:35 +0300

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,19 @@
-
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* main - lowercase and uppercase letters
+* Return: success-0
+*/
+int main(void)
+{
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+if (ch == q || ch == e)
+{
+putchar(ch,' ');
+putchar('\n');
+}
+}
+return (0);
+}
