ab426f5aefb073049dd685ebcd71eebddd33c164
kalkidan999 <kalkidandemes99@gmail.com>
2021-02-04 03:18:29 +0000

    no printf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
 int main(void)
 {
 char ch;
-ch="and that piece of art is useful" - Dora Korpar, 2015-10-19";
-putc(cu,stdout);
+ch = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+putc(ch, stdout);
 return (1);
 }
