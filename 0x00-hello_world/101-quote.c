99e5332a5961011fda4efbbb36c068f60c5217ae
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-04 09:12:31 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,6 +10,6 @@ int main(void)
 }
 int func(void)
 {
-printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+write("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 return (1);
 }
