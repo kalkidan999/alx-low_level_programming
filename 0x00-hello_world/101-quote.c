16049d7a1be28f03f3c477a46b678c86c80d98cc
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-04 06:47:24 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
-#define fun main
-/*
+/**
  * main 
  *
  * Return:1
  */
+int main(void)
+{
+/* some codes here*/
+}
 int func(void)
 {
-print("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 return (1);
 }
