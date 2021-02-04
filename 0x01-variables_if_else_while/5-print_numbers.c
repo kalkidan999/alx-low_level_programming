b4cb5d610118eac5e9ce506f84d7ef407400c944
kalkidan999 <kalkidandemes99@gmail.com>
2021-02-04 18:46:04 +0000

    print no

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,10 +3,12 @@
 * main - single digits
 * Return: last digit
 */
-int main(void){
+int main(void)
+{
 int i;
-for (i = 0; i <= 9; i % 10)
-i++;
+for (i = 0; i <= 9; i++)
+{
 printf("%d\n", i);
+}
 return (0);
 }
