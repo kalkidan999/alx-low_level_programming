25e9fed2ebe34294ca7a18cd9eb91d7ca77527a3
kalkidan999 <kalkidandemes99@gmail.com>
2021-02-04 21:02:16 +0000

    base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,1 +1,15 @@
-
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* main - base16 lowercase letters
+* Return: success-0
+*/
+int main(void)
+{
+char hex = 0123456789;
+char hex1 = abcdef;
+putchar(hex + '0');
+putchar(hex1);
+putchar('\n');
+return (0);
+}
