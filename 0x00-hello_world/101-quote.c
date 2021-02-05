9eb94ca4019ae3b63e3573e5f1b8e5ae6b39b31c
kalkidan999 <kalkidandemes99@gmail.com>
2021-02-05 18:01:46 +0000

    dora

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
- * main 
- *
+ * main - main
  * Return:1
  */
 int main(void)
 {
-/* some codes here*/
-}
-int func(void)
-{
-write(1,"and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
