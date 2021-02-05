aa67b02ad347eea0f9d8805d43f4db53ea27d985
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-05 15:55:32 +0300

    Create 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h> 
+/**
+* main - random numbers
+* main - combinationUtil
+* Return: last digit
+*/
+void combinationUtil(int arr[], int data[], int start, int end, int index, int r); 
+void printCombination(int arr[], int n, int r) 
+{
+int data[r];
+combinationUtil(arr, data, 0, n-1, 0, r); 
+}
+void combinationUtil(int arr[], int data[], int start, int end, int index, int r) 
+{
+if (index == r) 
+{ 
+for (int j=0; j<r; j++) 
+printf("%d ", data[j]); 
+printf("\n"); 
+return; 
+}
+for (int i=start; i<=end && end-i+1 >= r-index; i++) 
+{ 
+data[index] = arr[i]; 
+combinationUtil(arr, data, i+1, end, index+1, r); 
+} 
+}
+int main(void) 
+{ 
+int arr[] = {0,1}; 
+int r = 2; 
+int n = sizeof(arr)/sizeof(arr[0]); 
+printCombination(arr, n, r); 
+} 
