8bf599064ad1cb8f2d7fafa057f011f4ab3c3e81
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-05 19:45:23 +0300

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,12 +11,15 @@ for (x = '0'; x <= '9'; x++)
 {
 for (y = '0'; y <= '9'; y++)
 {
+if (x < y)
+{
 putchar(x);
 putchar(y);
-if (!(x == '9' && y == '9' && x > y))
+if (!(x == '8' && y == '9'))
 {
 putchar(',');
 putchar(' ');
+}
 }
 }
 }
