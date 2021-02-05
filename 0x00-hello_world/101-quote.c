30dea63ddb1f999a0b92d7fafee55b18d2c289b5
kalkidan999 <kalkidandemes99@gmail.com>
2021-02-05 18:14:34 +0000

    dora

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,21 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
-<<<<<<< HEAD
- * main - main
+ *main - main
  * Return:1
  */
 int main(void)
 {
 write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-=======
-* main 
-*
-* Return:1
-*/
-int main(void)
-{
-write(1,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
->>>>>>> 044c3723a1f788f4b4642f090dc94f953297c291
 return (1);
 }
