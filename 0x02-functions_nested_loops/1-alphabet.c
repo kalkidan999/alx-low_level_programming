45bea5ccd9fbbf71c1ce90feb1d812652ed830e5
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-09 00:09:38 +0300

    Lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,12 @@
 #include "holberton.h"
-
 /**
  * main - check the code for Holberton School students.
- *
  * Return: Always 0.
  */
 void print_alphabet(void)
 {
-  char ch;
-  for (ch = 'a' ; ch <= 'z' ; ch++)
-    _putchar(ch);
-  _putchar('\n');
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+_putchar(ch);
+_putchar('\n');
 }
