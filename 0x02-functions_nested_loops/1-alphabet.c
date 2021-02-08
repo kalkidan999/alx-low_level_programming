2dba815683c5e2cd16bd51cbd97c03d8788fd705
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-09 00:15:31 +0300

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * main - check the code for Holberton School students.
+ * print_alphabet - check the code for Holberton School students.
  * Return: Always 0.
  */
 void print_alphabet(void)
