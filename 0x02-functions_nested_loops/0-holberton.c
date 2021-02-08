a070452a4ad265173154eadd11433214eb32267d
kalkidan999 <kalkidandemes99@gmail.com>
2021-02-08 06:59:08 +0000

    functions

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,5 +1,10 @@
 #include<studio.h>
+/**
+ * main - random numbers
+ * Return: 1 if the number is positive. 0 otherwise
+ */
 int main(void)
 {
-Write(1,"Holberton\n",10);
+Write(1, "Holberton\n", 10);
+return (0);
 }
