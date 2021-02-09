39c418bb46c37f1586848fba9f155d097e5eb703
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-09 15:23:24 +0300

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,1 +1,22 @@
-
+#include "holberton.h"
+/**
+* times_table - calculates time table
+*  its mine damnit
+*
+* Time table.
+*/
+void times_table(void)
+{
+int i;
+int j;
+int y;
+for (i = 0; i <= 9; i++)
+{
+for (j = 0; j <= 9; j++)
+{
+y = i * j;
+_putchar(y);
+}
+_putchar('\n');
+}
+}
