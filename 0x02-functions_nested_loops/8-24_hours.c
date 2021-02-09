1b080a9ca9325999bd32cf4f5a99a32637991d66
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-09 14:53:22 +0300

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,19 +8,21 @@ int HourTen;
 int HourOne;
 int MinuteTen;
 int Minuteone;
-for (HourTen = '0'; HourTen <= '2'; HourTen++)
+for (HourTen = '0'; HourTen <= '9'; HourTen++)
 {
-for (HourOne = '0'; HourOne <= '3'; HourOne++)
+for (HourOne = '0'; HourOne <= '9'; HourOne++)
 {
 for (MinuteTen = '0'; MinuteTen <= '5'; MinuteTen++)
 {
 for (Minuteone = '0'; Minuteone <= '9'; Minuteone++)
 {
-if (MinuteTen == 5 && Minuteone == 9 && HourOne == 3)
+if (MinuteTen == 5 && Minuteone == 9)
 break;
 else
 {
 _putchar (HourTen);
+if (HourTen > 2 && HourOne >3)
+continue;
 _putchar (HourOne);
 _putchar (':');
 _putchar (MinuteTen);
