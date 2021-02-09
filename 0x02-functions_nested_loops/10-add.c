4cd1f1cd8fa587150b0312461082c86a0c8ed793
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-09 15:20:22 +0300

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,1 +1,14 @@
-
+#include "holberton.h"
+/**
+* add - addition
+* @a: adds two inputs
+* @b: add two inputs
+*
+* Return: sum
+*/
+int add(int a, int b)
+{
+int sum;
+sum = a + b;
+return (sum);
+}
