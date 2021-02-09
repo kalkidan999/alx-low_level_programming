a0ec20dae84c946b860914efe9becd4af3757802
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-09 20:59:51 +0300

    Create 100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+* print_times_table - printing
+* @n: n
+*/
+void print_times_table(int n)
+{
+int i;
+int j;
+if (n < 15)
+{
+for (i = 0; i <= 15; i++)
+{
+if (i != 15)
+printf("%d, ", i);
+else
+printf("%d", i);
+}
+printf("\n");
+}
+else if (n == 15)
+{
+printf("%d", n);
+printf("\n");
+}
+else if (n > 15 || n < 0)
+{
+break;
+}
+printf("\n");
+}
+}
