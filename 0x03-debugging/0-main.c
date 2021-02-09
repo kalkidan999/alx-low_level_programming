be4c64f948194894744cc18efd4a5634bfb0bcc7
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-09 16:24:28 +0300

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,1 +1,23 @@
-
+#include <holberton.h>
+/**
+* positive_or_negative - random numbers
+*@i: integer
+* Return: 1 if the number is positive. 0 otherwise
+*/
+void positive_or_negative(int i)
+{
+int n;
+if (n > 0)
+{
+printf("%d is positive\n", n);
+}
+else if (n == 0)
+{
+printf("%d is zero\n", n);
+}
+else
+{
+printf("%d is negative\n", n);
+}
+return (0);
+}
