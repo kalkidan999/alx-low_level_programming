649c9d2821d23ae624d97b5c8ec83ef3cc25b1bd
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-09 10:47:28 +0300

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,16 +1,17 @@
 #include "holberton.h"
-#include <ctype.h>
 /**
  * main - check the code for Holberton School students.
- *
+ *@c: c class
  * Return: Always 0.
  */
 int _isalpha(int c)
 {
-  if (isalpha(c)){
-    return (1);
-  }else
-    {
-      return(0);
-    }
+if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+{
+return (1);
+}
+else
+{
+return(0);
+}
 }
