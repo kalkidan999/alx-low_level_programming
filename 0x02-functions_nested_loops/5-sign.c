21831ba5984672754b892ea31b9fd181d163faac
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-09 10:59:31 +0300

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,24 +1,24 @@
 #include "holberton.h"
-
 /**
- * main - check the code for Holberton School students.
- *
+ * print_sign - check the code for Holberton School students.
+ * @n: integer
  * Return: Always 0.
  */
 int print_sign(int n)
 {
-  if (n>0)
-    {
-      _putchar('+');
-      return (1);
-
-    }else if (n ==0)
-    {
-      _putchar('0');
-      return (0);
-    }else{
-    _putchar('-');
-    return (-1);
-  }
-
+if (n>0)
+{
+_putchar('+');
+return (1);
+}
+else if (n ==0)
+{
+_putchar('0');
+return (0);
+}
+else
+{
+_putchar('-');
+return (-1);
+}
 }
