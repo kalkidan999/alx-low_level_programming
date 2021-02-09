a7a56c457b1830374f79f5b6e6ac1a35a3ff75c4
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-09 11:06:39 +0300

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,1 +1,13 @@
-
+#include "holberton.h"
+/**
+* print_last_digit - does my stuff
+* @n: its mine damnit
+*
+* Return: last digit.
+*/
+int print_last_digit(int n)
+{
+int Last;
+Last = n % 10;
+return (Last);
+}
