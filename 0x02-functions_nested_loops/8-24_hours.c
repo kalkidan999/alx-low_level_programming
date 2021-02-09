b15423cc25ee020d32185401e45d10a1b6a54f85
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-09 11:55:01 +0300

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,23 +1,20 @@
 #include "holberton.h"
-# /**
-#  * my_function - does my stuff
-#  * @my_arg: its mine damnit
-#  *
-#  * Does my stuff explained.
-#  */
+/**
+ * jack_bauer - timing
+ */
 void jack_bauer(void)
 {
 int HourTen = '0';
 int HourOne = '0';
 int MinuteTen = '0';
 int Minuteone = '0';
-for (HourTen = '0'; HourTen <= '9'; HourTen++)/* prints hours ten digit*/
+for (HourTen = '0'; HourTen <= '9'; HourTen++)/
 {
-for (HourOne = '0'; HourOne <= '9'; HourOne++)/* prints Hours one digit*/
+for (HourOne = '0'; HourOne <= '9'; HourOne++)
 {
-for (MinuteTen = '0'; MinuteTen <= '5'; MinuteTen++)/* prints minutes ten digit*/
+for (MinuteTen = '0'; MinuteTen <= '5'; MinuteTen++)
 {
-for (Minuteone = '0'; Minuteone <= '9'; Minuteone++)/* prints minutes one digit*/
+for (Minuteone = '0'; Minuteone <= '9'; Minuteone++)
 {
 if (MinuteTen == 5 && Minuteone == 9)
 break;
