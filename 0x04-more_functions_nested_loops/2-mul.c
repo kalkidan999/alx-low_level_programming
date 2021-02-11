9dfb0afb810d0f43d017f4d3ec68f6b824104aa7
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-11 12:09:38 +0300

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,1 +1,11 @@
-
+#include "holberton.h"
+/**
+ * _isdigit - check the code for Holberton School students.
+ * @a: character a
+ * @b: character b
+ * Return: 1 and 0
+ */
+int mul(int a, int b)
+{
+_putchar(a * b);
+}
