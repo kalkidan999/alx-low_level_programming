5111733142653d920b9607144d46d4b9d0bf71cb
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-11 22:12:38 +0300

    Printing fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,17 +10,21 @@ int main(void)
 int i;
 for (i = 1; i <= 100; i++)
 {
-if ((i % 3) == 0)
+if (i == 100)
+{
+printf("Buzz");
+}
+else if ((i % 3) == 0 && (i % 5) == 0)
+{
+printf("FizzBuzz ");
+}
+else if ((i % 3) == 0)
 {
 printf("Fizz ");
 }
 else if ((i % 5) == 0)
 {
 printf("Buzz ");
-}
-else if (while ((i % 3) && (i % 5) == 0))
-{
-printf("FizzBuzz ");
 }
 else
 {
