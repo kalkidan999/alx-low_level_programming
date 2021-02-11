485435b3c6610bdb89bbc27cf95d41c551fce9c5
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-11 20:24:57 +0300

    Update 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,1 +1,24 @@
-
+#include "holberton.h"
+/**
+* print_square -checks for upper case
+*
+* @size: size
+* Return: 1 
+*/
+void print_square(int size)
+{
+int i;
+int j;
+if (size == 0)
+{
+_putchar('\n');
+}
+for (i = 1; i <= size; i++)
+{
+for (j = 1; j <= size; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
