966f8ace8f3e6ea8063b280e1521056f0ac600a9
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-11 17:49:01 +0300

    Update 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,1 +1,14 @@
-f
+#include "holberton.h"
+/**
+* print_diagonal - check the code for Holberton School students.
+* @n: integer
+* Return: Always 0.
+*/
+void print_diagonal(int n)
+{
+int i;
+for (i = 0; i < n; ++i)
+_putchar('\');
+_putchar('\n');
+return;
+}
