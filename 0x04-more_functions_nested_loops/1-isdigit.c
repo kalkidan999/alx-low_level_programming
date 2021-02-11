c2468aedf0490c73430f925f04a63fbe5f11304b
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-11 11:46:35 +0300

    Update 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,1 +1,12 @@
-
+#include "holberton.h"
+/**
+ * _isdigit - check the code for Holberton School students.
+ * @c: characters
+ * Return: 1 and 0
+ */
+int _isdigit(int c)
+{
+if (c >= 0 && c <= 9)
+return (1);
+return (0);
+}
