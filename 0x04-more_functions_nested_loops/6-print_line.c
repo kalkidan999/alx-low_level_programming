72ce99fb9e73f86d81f4d045f8c352c07ef17b58
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-11 17:36:30 +0300

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,1 +1,12 @@
-d
+#include "holberton.h"
+/**
+* print_line - check the code for Holberton School students.
+* @n: integer
+* Return: Always 0.
+*/
+void print_line(int n)
+{
+if (n > 0)
+_putchar('_');
+return;
+}
