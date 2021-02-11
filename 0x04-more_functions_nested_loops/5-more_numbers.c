944c0e86abada3f18709bc4733fcddda62ceae18
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-11 15:18:21 +0300

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,1 +1,17 @@
-
+#include "holberton.h"
+/**
+ * print_most_numbers - check the code for Holberton School students
+ * Return: success
+ */
+void print_most_numbers(void)
+{
+int i;
+int j;
+for (i = 0; i <= 14; i++)
+{
+for (j = 0; j <= 14; j++)
+_putchar('0' + j);
+}
+_putchar('\n');
+return;
+}
