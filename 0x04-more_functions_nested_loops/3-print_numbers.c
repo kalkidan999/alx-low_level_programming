b7a8983a091a234ff03c9786a156e268a629ca51
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-11 14:26:30 +0300

    0 to 9 numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,1 +1,16 @@
-
+#include "holberton.h"
+/**
+ * mul - check the code for Holberton School students.
+ * @a: character a
+ * @b: character b
+ * Return: success
+ */
+void print_numbers(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+{
+_putchar('0' + i);
+}
+_putchar('\n');
+}
