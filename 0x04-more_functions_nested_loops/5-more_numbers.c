7ba9d9a365a3a3cc67bcd07832babe5dd7b8e187
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-11 15:31:22 +0300

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,13 +7,13 @@ void more_numbers(void)
 {
 int i;
 int j;
-for (i = 0; i <= 10; i++)
+for (i = 0; i <= 14; i++)
 {
-for (j = 0; j <= 14; j++)
+for (j = 0; j <= 10; j++)
 {
 _putchar('0' + j);
-}
 _putchar('\n');
+}
 _putchar('0' + i);
 }
 return;
