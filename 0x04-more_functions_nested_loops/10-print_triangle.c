014b28bb29db35af52f2690cfd7ba9305af45948
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-11 23:17:41 +0300

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,1 +1,34 @@
-d
+#include "holberton.h"
+/**
+* print_triangle -checks for upper case
+*
+* @size: returns size
+* Return: 1 for digit and 0 for else
+*/
+void print_triangle(int size)
+{
+int i;
+int j;
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+if (j < size - i - 1)
+{
+_putchar(' ');
+}
+else
+{
+_putchar('#');
+}
+}
+_putchar('\n');
+}
+}
+}
