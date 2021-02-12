e37a89a39f2af6724724c6dee9b1d4111e779ad6
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-12 16:00:25 +0300

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -11,10 +11,16 @@ if (n < 0)
 _putchar('-');
 n = -n;
 }
-if (n / 10 != 0)
+if (n / 10 != 0 && n < 100)
 {
 _putchar('0' + n / 10);
+}
+else
+{
 _putchar('0' + n / 100);
+}
+else
+{
 _putchar('0' + n / 1000);
 }
 _putchar(n % 10 + '0');
