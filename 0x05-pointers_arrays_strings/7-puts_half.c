2803c118fb24b96f4daf82b3d70cb9c2f9bdbdc1
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-15 23:02:09 +0300

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,15 +8,16 @@ void puts_half(char *str)
 {
 int i, c = 0;
 int n = (c - 1) / 2;
+int m = c / 2;
 for (i = 0; *(str + i) != '\0'; i++)
 { 
 c++;
 } 
 if ((c / 2) == 0)
 {
-for (i = 0; i <= c; i ++)
+for (i = m; i <= c; i ++)
 {
-_putchar(str[i + 5]);
+_putchar(str[i]);
 }
 }
 else
