42b85c62d2fbadaee1a4e443fd696ed2a0e16760
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-15 22:23:23 +0300

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,1 +1,19 @@
-
+#include "holberton.h"
+/**
+* puts_half -change value using the pointer address
+*
+*@str: stores the input
+*/
+void puts_half(char *str)
+{
+int i/2, c = 0;
+for (i = 0; *(str + (i) != '\0'; i++)
+{ 
+c++;
+} 
+for (i = 0; i <= c; i += 2)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
