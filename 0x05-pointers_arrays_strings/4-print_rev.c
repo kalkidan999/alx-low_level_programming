3620a2f19bb4f5dd53cb256642ddc51aed32ff23
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-15 17:01:05 +0300

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,19 +1,26 @@
 #include "holberton.h"
 /**
- * print_rev - check the code for Holberton School students.
- * @*s: integer1
- * Return: Always 0.
- */
+* print_rev -change value using the pointer address
+*
+*@s: stores the input
+*/
 void print_rev(char *s)
 {
-char temp;
-int length = -1;
-while (length >= 0)
+char revstr[100];
+char *rvptr = revstr;
+int i = -1;
+while(*s)
 {
-temp = *s;
-_putchar(temp);
 s++;
---length;
+i++;
+}
+while(i >= 0)
+{
+s--;
+*rvptr = *s;
+_putchar(*rvptr);
+rvptr++;
+--i;
 }
 _putchar('\n');
 }
