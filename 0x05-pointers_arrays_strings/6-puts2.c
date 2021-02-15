2eb8b36088cb015ef8ec804ec615f80eb91d56d3
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-15 22:17:25 +0300

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,7 +8,6 @@
 void puts2(char *str)
 {
 int i, c = 0;
-char s[50] = "";
 for (i = 0; *(str + i) != '\0'; i++)
 { 
 c++;
@@ -16,8 +15,6 @@ c++;
 for (i = 0; i <= c; i += 2)
 {
 _putchar(str[i]);
-s[i/2]=str[i];
 }
 _putchar('\n');
-printf("\n\n2nd option\n%s", s);
 }
