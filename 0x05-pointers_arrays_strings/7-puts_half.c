26a0781c03026224d24066546f9e58bc7c18ec71
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-15 22:44:51 +0300

    Create 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,14 +6,24 @@
 */
 void puts_half(char *str)
 {
-int i/2, c = 0;
+int i, c = 0;
 for (i = 0; *(str + (i) != '\0'; i++)
 { 
 c++;
 } 
+if ((c / 2) = 0)
+{
 for (i = 0; i <= c; i += 2)
 {
-_putchar(str[i]);
+_putchar(str[i + 5]);
+}
+}
+else
+{
+int n = (c - 1) / 2
+for (i = 0; i <= n; i += 2)
+{
+_putchar(str[i + 5]);
 }
 _putchar('\n');
 }
