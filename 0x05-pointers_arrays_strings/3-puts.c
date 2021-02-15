41362e91dcdb67e88243d991e3051bfb06f904dc
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-15 14:36:09 +0300

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,14 +1,10 @@
 #include "holberton.h"
 /**
- * _strlen - check the code for Holberton School students.
+ * _puts - check the code for Holberton School students.
  * @*s: integer1
  * Return: Always 0.
  */
-int _strlen(char *s)
+void _puts(char *str)
 {
-int count;
-int n = s;
-while (*n != '\0')
-count++;
-s++;
+_putchar('str');
 }
