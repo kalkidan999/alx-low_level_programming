3165933cf2aa444622f3dacd2484944887e26671
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-15 15:49:53 +0300

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,1 +1,15 @@
-
+#include "holberton.h"
+#include <string.h>
+/**
+ * print_rev - check the code for Holberton School students.
+ * @*s: integer1
+ * Return: Always 0.
+ */
+void print_rev(char *s)
+{
+int i, j = strlen(str);
+for(i = 0; i < j; i++)
+print_rev(str + 1);
+_putchar(*str++);
+_putchar('\n');
+}
