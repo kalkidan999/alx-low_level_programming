f697f350f47723ceb659db036339acda66190501
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-15 23:33:50 +0300

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,1 +1,13 @@
-
+#include "holberton.h"
+/**
+* print_array -change value using the pointer address
+*
+*@str: stores the input
+*@n: integer
+*/
+void print_array(int *a, int n)
+{
+int *a;
+int t[n];
+a = t;
+}
