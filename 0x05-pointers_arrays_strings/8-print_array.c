8b3d456d8377971310714f68a4549d8f6f5ef293
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-16 00:08:47 +0300

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,6 +11,11 @@ void print_array(int *a, int n)
 int i;
 for(i=0;i<n;i++)
 {
-printf("%d, \n", a[i]);
+printf("%d ", a[i]);
 } 
+for(i=0;i<n;i++)
+{
+printf(",", a[i]);
+} 
+printf("\n", a[i]);
 }
