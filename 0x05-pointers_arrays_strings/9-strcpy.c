d450defe4db8743f9159969e831261554ab95206
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-16 01:15:16 +0300

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,1 +1,18 @@
-a
+#include "holberton.h"
+#include <stdio.h>
+/**
+* *_strcpy -change value using the pointer address
+*
+*@dest: stores the input
+*@src: integer
+*/
+char *_strcpy(char *dest, char *src)
+{
+while(*src)
+{
+*dest = *src;        
+src++;
+dest++;
+}    
+*dest = '\0';
+}
