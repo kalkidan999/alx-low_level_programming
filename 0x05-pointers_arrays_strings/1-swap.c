d76465030fb7988d9ac629319b8e99a686e3c7bc
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-15 12:15:28 +0300

    Update 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,1 +1,13 @@
-
+#include "holberton.h"
+/**
+ * reset_to_98 - check the code for Holberton School students.
+ * @*a: integer1
+ * @*b: integer2
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+int n;
+*n = a;
+*n = b;
+}
