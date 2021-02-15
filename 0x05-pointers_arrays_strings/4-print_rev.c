f86c1dbc2d035d3524ddf206deff8d78802b5694
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-15 16:53:51 +0300

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,14 +6,17 @@
  */
 void print_rev(char *s)
 {
+char *revPtr;
 char temp;
 int length = -1;
 while (*s != '\0')
 {
 temp = *s;
 _putchar(temp);
-length++;
 s--;
+*revPtr = *s;
+revPtr++;
+--length;
 }
 _putchar('\n');
 }
