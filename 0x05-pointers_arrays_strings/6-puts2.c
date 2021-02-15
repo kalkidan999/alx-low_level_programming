b33e4923a75fb26a3f112c536ed293facf684ac8
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-15 17:09:11 +0300

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,1 +1,26 @@
-a
+#include "holberton.h"
+/**
+* rev_string -change value using the pointer address
+*
+*@s: stores the input
+*/
+void rev_string(char *s)
+{
+char revstr[100];
+char *rvptr = revstr;
+int i = -1;
+while(*s)
+{
+s++;
+i++;
+}
+while(i >= 0)
+{
+s--;
+*rvptr = *s;
+_putchar(*rvptr);
+rvptr++;
+--i;
+}
+_putchar('\n');
+}
