cd9f50de5454b4f1ca6e8c905ba91642718ada83
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-16 09:42:31 +0300

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
  * _strlen - check the code for Holberton School students.
- * @*s: integer1
+ * @s: integer1
  * Return: Always 0.
  */
 int _strlen(char *s)
@@ -12,5 +12,5 @@ while (*s != '\0')
 count++;
 s++;
 }
-return count;
+return (count);
 }
