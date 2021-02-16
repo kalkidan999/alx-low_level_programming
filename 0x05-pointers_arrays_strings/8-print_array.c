aad40d8cea43769ffc2960bd65c8e609955905ed
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-16 10:05:10 +0300

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -3,19 +3,19 @@
 /**
 * print_array -change value using the pointer address
 *
-*@str: stores the input
+*@a: stores the input
 *@n: integer
 */
 void print_array(int *a, int n)
 {
 int i;
-for(i=0;i<n;i++)
+for (i = 0; i < n; i++)
 {
 printf("%d", a[i]);
 if (i != (n - 1))
 {
 printf(", ");
 }
-} 
+}
 printf("\n");
 }
