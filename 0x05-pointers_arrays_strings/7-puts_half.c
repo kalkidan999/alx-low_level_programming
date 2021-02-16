0b17ac516aa4d2ce55649bb0658369bc06592531
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-16 11:20:07 -0800

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,14 +12,14 @@ while (str[c])
 c++;
 }
 r = c % 2;
-for (i = c / 2  ; i < c; i++)
+for (i = c / 2; i < c; i++)
 {
 if (r == 0)
 {
 _putchar(str[i]);
 }
 }
-for (i = (c - 1) / 2 ; i < c; i++)
+for (i = (c - 1) / 2; i < c; i++)
 {
 if (r == 1)
 {
