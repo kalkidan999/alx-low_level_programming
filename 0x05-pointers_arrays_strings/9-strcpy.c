9960c99ea4a92f75c2f69bd268faa2769cf3b45e
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-16 23:23:13 +0300

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -4,13 +4,13 @@
 *@dest: stores the input
 *@src: integer
 */
-char *_strcpy(char *dest, char *src)
+char *_strcpy(char *dest, const char *src)
 {
 int index = 0;
-while(src[index])
+while (src[index])
 {
-dest[index] = src[index];        
-indexx++;
+dest[index] = src[index];
+index++;
 }
 dest[index]++ = '\0';
 return (dest);
