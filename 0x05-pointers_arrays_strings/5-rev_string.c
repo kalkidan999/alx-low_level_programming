b4fe24f35383f52f75ce3d57bc7d696e11f11b96
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-16 08:33:55 -0800

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,11 +7,12 @@
 void rev_string(char *s)
 {
 int i, c = 0;
+i = c - 1;
 for (i = 0; *(s + i) != '\0'; i++)
 {
 c++;
 }
-for (i = c - 1; i >= 0; i--)
+for (i ; i >= 0; i--)
 {
 _putchar(*(s + i));
 }
