d3ab3ecb60a2bc131ba07dd6f7dc60b68a950af3
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-16 08:46:27 -0800

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,3 +12,15 @@ char *first = s, *last = s, temp;
 while (s[c])
 c++;
 }
+for (i = 0; i < c - 1; i++)
+{
+last++;
+}
+for (i = 0; i < c--; i++)
+{
+temp = *last;
+*last = *first;
+*first = temp;
+first++;
+last--;
+}
