4044ec020b1677703a7682158fa3e9b370a4e74b
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-17 15:02:58 +0300

    Create 100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+*_atoi - change value using
+*
+*@s: character
+*Return: int
+*/
+int _atoi(char *s)
+{
+int res = 0;
+int sign = 1;
+int i = 0;
+if (str[0] == '-')
+{
+sign = -1;
+i++;
+}
+for (; str[i] != '\0'; ++i)
+res = res * 10 + str[i] - '0';
+return sign * res;
+}
+}
