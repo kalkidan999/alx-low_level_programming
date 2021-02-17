2ddede3516588aad7e8a775bea859a1382c79ae1
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-17 15:53:07 +0300

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,15 +7,15 @@
 */
 int _atoi(char *s)
 {
-unsigned int res = 0;
 int sign = 1;
-int i = 0;
-if (s[0] == '-')
-{
-sign = -1;
-i++;
-}
-for (; s[i] != '\0'; ++i)
-res = res * 10 + s[i] - '0';
-return (sign *res);
+unsigned int num = 0;
+do {
+if (*s == '-')
+sign *= -1;
+else if (*s >= '0' && *s <= '9')
+num = (num * 10) + (*s - '0');
+else if (num > 0)
+break;
+} while (*s++);
+return (num *sign);
 }
