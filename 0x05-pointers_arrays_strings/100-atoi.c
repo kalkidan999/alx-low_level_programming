b166634ae42294d971e8596ba1b561d117784978
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-17 15:06:52 +0300

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,13 +10,13 @@ int _atoi(char *s)
 int res = 0;
 int sign = 1;
 int i = 0;
-if (str[0] == '-')
+if (s[0] == '-')
 {
 sign = -1;
 i++;
 }
-for (; str[i] != '\0'; ++i)
-res = res * 10 + str[i] - '0';
-return sign * res;
+for (; s[i] != '\0'; ++i)
+res = res * 10 + s[i] - '0';
+return (sign * res);
 }
 }
