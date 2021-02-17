b5270c65679411e05c1a5b8fab8ab72b38ad4a66
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-17 18:59:49 +0300

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include <time.h>
 
-int main(void) 
+int main()
 {
-     srand((unsigned int)(time(NULL)));
-    char characters[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789/,.-+=~`<>:";
+    srand((unsigned int)(time(NULL)));
+    int i;
+    char pass[12];
 
-    for (int index = 0; index < 12; index++)
-    {
-        printf("%c", characters[rand() % (sizeof characters - 1)]);
+    printf("Press enter to get a twelve-character password\n");
+    getchar();
+
+    for (i = 0; i < 4; i++) {
+        pass[i] = rand() % 9;
+        char capLetter = 'A' + (rand() % 26);
+        pass[i + 2] = capLetter;
+        char letter = 'a' + (rand() % 26);
+        pass[i + 3] = letter;
+        printf("%d%c%c", pass[i], pass[i + 2], pass[i + 3]);
     }
-return (0);
+    printf("\n\n");
 }
