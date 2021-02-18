b5a84e07328afb3237026f39a2061be2e5987e37
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-18 10:58:12 +0300

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,1 +1,31 @@
+#include "holberton.h"
+/**
+*_strcat - change value using
+*
+*@dest: stores the input
+*@src: integer
+*Return: int
+*/
+char *_strcat(char *dest, char *src)
+{
+int i, j, c = 0, d = 0;
+i = 0;
+while (dest[c])
+{
+c++;
+}
+  while (src[d])
+{
+c++;
+}
+for (i = 0; i < c; i++);
+  
+  	for (j = 0; i < d; j++, i++)
+  	{
+  		dest[i] = src[j];
+  	}
+   	src[i] = '\0';
 
+  	_putchar(src[i]);
+  	
+}
