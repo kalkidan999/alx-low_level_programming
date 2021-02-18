669b9a92404548406f3a16787dbb8fcc6bc6db0d
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-18 11:57:44 -0800

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,1 +1,20 @@
-l
+#include "holberton.h"
+/**
+* cap_string -  str
+*
+*@s: charactee
+*Return: success
+*/
+char *cap_string(char *s)
+{
+int c = 0;
+while (s[c] != '\0')
+{
+if (s[c] >= 'a' && s[c] <= 'z')
+{
+s[c] = s[c] - 32;
+}
+c++;
+}
+return (s);
+}
