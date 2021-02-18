0bac5ab0a46b2638a970a724a2c71e0cf63c49cc
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-18 08:25:36 -0800

    Update 8-rot13.c

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -1,1 +1,18 @@
+#include "holberton.h"
+/**
+*rot13 - change value using
+*
+*Return: int
+*/
+char *rot13(char *s)
+{
+int i = 0;
+        for( i = 0; s[ i ] != '\0' ; i++ ){
 
+            if( *( s + i ) >= 'a' && *( str + i ) < 'n')
+                *( s + i ) += 13;       
+
+            else if( *( str + i ) >= 'n' && *( str + i ) <= 'z')
+                *( str + i ) -= 13;
+        }
+}
