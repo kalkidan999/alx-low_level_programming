d2344b27c34b73caf49b4fa525f47cecda6d70a6
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-18 11:08:50 -0800

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,1 +1,20 @@
-
+#include "holberton.h"
+/**
+* string_toupper -conc str
+*
+*@s: charactee
+*Return: success
+*/
+char *string_toupper(char *s)
+{
+int c = 0;
+while (s[c] != '\0')
+{
+if (s[c] >= 'a' && s[c] <= 'z')
+{
+s[c] = s[c] - 32;
+}
+c++;
+}
+return (s);
+}
