6455a89e344a9f7ec84ee3b8f27b84e2d73a375d
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-18 08:43:04 -0800

    Update 8-rot13.c

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -1,27 +1,24 @@
 #include "holberton.h"
 /**
 *rot13 - change value using
-*
+*s: character
 *Return: int
 */
 char *rot13(char *s)
 {
 int i;
-
-	i = 0;
-	while (s[i] != '\0')
-	{
-		while ((s[i] >= 'a' && s[i] <= 'z') ||
-		       (s[i] >= 'A' && s[i] <= 'Z'))
-		{
-			if ((s[i] >= 'N' && s[i] <= 'Z') ||
-			    (s[i] >= 'n' && s[i] <= 'z'))
-				s[i] -= 13;
-			else
-				s[i] += 13;
-			i++;
-		}
-		i++;
-	}
-	return (s);
+i = 0;
+while (s[i] != '\0')
+{
+while ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
+{
+if ((s[i] >= 'N' && s[i] <= 'Z') || (s[i] >= 'n' && s[i] <= 'z'))
+s[i] -= 13;
+else
+s[i] += 13;
+i++;
+}
+i++;
+}
+return (s);
 }
