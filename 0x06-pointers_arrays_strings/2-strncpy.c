01c0d2ada06c24a9c2970538221fd463a37a157a
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-18 10:57:55 -0800

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,1 +1,22 @@
-a
+#include "holberton.h"
+/**
+* _strncpy -concatinate string
+*
+*@dest: stores the input
+*@src: source
+*@n: number of characters
+*Return: dest
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+int j;
+i = 0;
+j = 0;
+for (i = 0; i <= n; i++)
+{
+dest[i] = src[j];
+j++;
+}
+return (dest);
+}
