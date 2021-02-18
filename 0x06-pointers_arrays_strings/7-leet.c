c23e0bff779e21111c605ce50331c39aefcc9d2f
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-18 10:51:01 -0800

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,19 +6,18 @@
  */
 char *leet(char *s)
 {
- int i;
- int j;
- char l[] = "ol_ea__t";
-
- for (i = 0; s[i] != '\0'; i++)
- {
-  for (j = 0; l[j] != '\0'; j++)
-  {
-   if (s[i] == l[j] || s[i] == (l[j] - 32))
-   {
-    s[i] = j + '0';
-   }
-  }
- }
- return (s);
+int i;
+int j;
+char l[] = "ol_ea__t";
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; l[j] != '\0'; j++)
+{
+if (s[i] == l[j] || s[i] == (l[j] - 32))
+{
+s[i] = j + '0';
+}
+}
+}
+return (s);
 }
