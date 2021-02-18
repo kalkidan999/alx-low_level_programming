70f0e9bb98ec86f3d6f8f23727ce150744f08c1a
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-18 16:47:23 +0300

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,1 +1,23 @@
-
+#include "holberton.h"
+/**
+*_strncat - change value using
+*n: integer
+*@dest: stores the input
+*@src: characteres
+*Return: int
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+while(*dest)
+	{
+		Str1++;
+	}
+	
+	while(*src)
+	{
+		*dest = *src;
+		*src++;
+		*dest++;
+  	}
+  	*dest = '\0';
+}
