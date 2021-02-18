bd8a547acd3d9c54af9c2c6e447da6125ba81a07
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-18 11:00:47 -0800

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,1 +1,24 @@
-
+#include "holberton.h"
+/**
+* _strcmp -concatinate string
+*
+*@s1: stores the input
+*@s2: source
+*Return: comp
+*/
+int _strcmp(char *s1, char *s2)
+{
+while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
+{
+s1++;
+s2++;
+}
+if (*s1 == *s2)
+{
+return (0);
+}
+else
+{
+return (*s1 - *s2);
+}
+}
