6d85a791ded3e7426897b680aa93f200e7831b30
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-18 11:04:33 -0800

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,1 +1,19 @@
-
+#include "holberton.h"
+/**
+* reverse_array -concatinate string
+*
+*@a: stores the input
+*@n: source
+*Return: comp
+*/
+void reverse_array(int *a, int n)
+{
+int i;
+int temp;
+for (i = 0; i < n / 2; i++)
+{
+temp = a[i];
+a[i] = a[n - 1 - i];
+a[n - 1 - i] = temp;
+}
+}
