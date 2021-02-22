f08f94c1c8a2c649e851d292a33ea8b3b3d08e43
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-22 16:58:15 +0300

    Update 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -11,11 +11,12 @@ char *_strpbrk(char *s, char *accept)
 unsigned int i, j;
 for (i = 0; s[i] != '\0'; i++)
 {
-for (j = 0; accept[j] != s[i]; j++)
+for (j = 0; accept[j] != '\0'; j++)
 {
-if (s[i] == accept[j])
+if (*s == accept[j])
 return (s);
 }
+s++;
 }
 return (s);
 }
