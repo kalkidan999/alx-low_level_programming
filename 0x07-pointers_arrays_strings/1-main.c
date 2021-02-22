0d1e7290254585c220ec196895303beaf172344d
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-22 06:06:28 +0300

    Create 1-main.c

---
+++ 0x07-pointers_arrays_strings/1-main.c
@@ -0,0 +1,196 @@
+
+0x07. C - Even more pointers, arrays and strings
+ Foundations - Low-level programming & Algorithm ― Hatching out
+
+ By Julien Barbier, co-founder & CEO at Holberton School
+
+ Ongoing project - started 02-22-2021, must end by 02-23-2021 (in about 18 hours) - you're done with 0% of tasks.
+
+ Checker will be released at 02-22-2021 12:00 PM
+
+ QA review fully automated.
+
+For this project, students are expected to look at this concept:
+
+Pointers and arrays
+
+
+Resources
+Read or watch:
+
+C - Pointer to Pointer
+C – Pointer to Pointer with example
+Multi-dimensional Arrays in C
+Two dimensional (2D) arrays in C programming with example
+Learning Objectives
+At the end of this project, you are expected to be able to explain to anyone, without the help of Google:
+
+General
+What are pointers to pointers and how to use them
+What are multidimensional arrays and how to use them
+What are the most common C standard library functions to manipulate strings
+Requirements
+General
+Allowed editors: vi, vim, emacs
+All your files will be compiled on Ubuntu 14.04 LTS
+Your programs and functions will be compiled with gcc 4.8.4 using the flags -Wall -Werror -Wextra and -pedantic
+All your files should end with a new line
+A README.md file, at the root of the folder of the project is mandatory
+Your code should use the Betty style. It will be checked using betty-style.pl and betty-doc.pl
+You are not allowed to use global variables
+No more than 5 functions per file
+You are not allowed to use the standard library. Any use of functions like printf, puts, etc… is forbidden
+You are allowed to use _putchar
+You don’t have to push _putchar.c, we will use our file. If you do it won’t be taken into account
+In the following examples, the main.c files are shown as examples. You can use them to test your functions, but you don’t have to push them to your repo (if you do we won’t take them into account). We will use our own main.c files at compilation. Our main.c files might be different from the one shown in the examples
+The prototypes of all your functions and the prototype of the function _putchar should be included in your header file called holberton.h
+Don’t forget to push your header file
+More Info
+You do not need to learn about pointers to functions, arrays of structures, malloc and free - yet.
+
+Quiz questions
+Show
+
+Tasks
+
+0. memset mandatory
+Write a function that fills memory with a constant byte.
+
+Prototype: char *_memset(char *s, char b, unsigned int n);
+The _memset() function fills the first n bytes of the memory area pointed to by s with the constant byte b
+Returns a pointer to the memory area s
+FYI: The standard library provides a similar function: memset. Run man memset to learn more.
+
+julien@ubuntu:~/0x07$ cat 0-main.c
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+void simple_print_buffer(char *buffer, unsigned int size)
+{
+        unsigned int i;
+
+        i = 0;
+        while (i < size)
+        {
+                if (i % 10)
+                {
+                        printf(" ");
+                }
+                if (!(i % 10) && i)
+                {
+                        printf("\n");
+                }
+                printf("0x%02x", buffer[i]);
+                i++;
+        }
+        printf("\n");
+}
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char buffer[98] = {0x00};
+
+    simple_print_buffer(buffer, 98);
+    _memset(buffer, 0x01, 95);
+    printf("-------------------------------------------------\n");
+    simple_print_buffer(buffer, 98);    
+    return (0);
+}
+julien@ubuntu:~/0x07$ gcc -Wall -pedantic -Werror -Wextra 0-main.c 0-memset.c -o 0-memset
+julien@ubuntu:~/0x07$ ./0-memset 
+0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00
+0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00
+0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00
+0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00
+0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00
+0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00
+0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00
+0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00
+0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00
+0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00
+-------------------------------------------------
+0x01 0x01 0x01 0x01 0x01 0x01 0x01 0x01 0x01 0x01
+0x01 0x01 0x01 0x01 0x01 0x01 0x01 0x01 0x01 0x01
+0x01 0x01 0x01 0x01 0x01 0x01 0x01 0x01 0x01 0x01
+0x01 0x01 0x01 0x01 0x01 0x01 0x01 0x01 0x01 0x01
+0x01 0x01 0x01 0x01 0x01 0x01 0x01 0x01 0x01 0x01
+0x01 0x01 0x01 0x01 0x01 0x01 0x01 0x01 0x01 0x01
+0x01 0x01 0x01 0x01 0x01 0x01 0x01 0x01 0x01 0x01
+0x01 0x01 0x01 0x01 0x01 0x01 0x01 0x01 0x01 0x01
+0x01 0x01 0x01 0x01 0x01 0x01 0x01 0x01 0x01 0x01
+0x01 0x01 0x01 0x01 0x01 0x00 0x00 0x00
+julien@ubuntu:~/0x07$ 
+Repo:
+
+GitHub repository: alx-low_level_programming
+Directory: 0x07-pointers_arrays_strings
+File: 0-memset.c
+
+1. memcpy mandatory
+Write a function that copies memory area.
+
+Prototype: char *_memcpy(char *dest, char *src, unsigned int n);
+The _memcpy() function copies n bytes from memory area src to memory area dest
+Returns a pointer to dest
+FYI: The standard library provides a similar function: memcpy. Run man memcpy to learn more.
+
+julien@ubuntu:~/0x07$ cat 1-main.c
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+void simple_print_buffer(char *buffer, unsigned int size)
+{
+    unsigned int i;
+
+    i = 0;
+    while (i < size)
+    {
+        if (i % 10)
+        {
+            printf(" ");
+        }
+        if (!(i % 10) && i)
+        {
+            printf("\n");
+        }
+        printf("0x%02x", buffer[i]);
+        i++;
+    }
+    printf("\n");
+}
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char buffer[98] = {0};
+    char buffer2[98] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+
+    simple_print_buffer(buffer, 98);
+    _memcpy(buffer + 50, buffer2, 10);
+    printf("-------------------------------------------------\n");
+    simple_print_buffer(buffer, 98);    
+    return (0);
+}
