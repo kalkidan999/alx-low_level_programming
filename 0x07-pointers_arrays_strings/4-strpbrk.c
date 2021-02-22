849fe0de37cf52e099976791c46d0e0ec3a193de
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-22 16:51:03 +0300

    Update 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,1 +1,21 @@
-
+#include "holberton.h"
+/**
+* _strpbrk -check character in a string
+*
+*@s: source
+*@accept: character
+*Return: number of characters
+*/
+char *_strpbrk(char *s, char *accept)
+{
+unsigned int i, j;
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; accept[j] != s[i]; j++)
+{
+if (accept[j] == '\0')
+return (s);
+}
+}
+return (s);
+}
