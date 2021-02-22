43c231d799ca12f03f3b19b4c245bacf332e5880
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-22 10:50:46 +0300

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,1 +1,18 @@
-
+#include "holberton.h"
+/**
+ * _strchr - searches buffer 
+ * @s: the pointer of memory to print
+ * @c: the character of memory to print
+ *
+ * Return: Nothing.
+ */
+char *_strchr(char *s, char c)
+{
+int i, count = 0;
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i] == c)
+count++;
+}
+return (s);
+}
