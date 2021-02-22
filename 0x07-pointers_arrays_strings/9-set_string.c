ad488aba669c50612210ae2500c2de392b09167a
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-22 22:01:12 +0300

    Update 9-set_string.c

--- 0x07-pointers_arrays_strings/9-set_string.c
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -1,1 +1,13 @@
-
+#include "holberton.h"
+#include <stdio.h>
+/**
+*set_string - print character in a string
+*
+*@s: print char
+*@to: sizes
+*Return: number of characters
+*/
+void set_string(char **s, char *to)
+{
+*s = to;
+}
