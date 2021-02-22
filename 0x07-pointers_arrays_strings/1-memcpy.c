93d190f41df4995df5863e5aa380579cc3464e18
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-22 07:17:59 +0300

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,6 +11,6 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 unsigned int index;
 for (index = 0; index < n; index++)
-dest[index] == src[index];
-return (s);
+dest[index] = src[index];
+return (dest);
 }
