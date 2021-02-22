ded56e401830960c514d79d4f103c95e37daa5d0
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-22 12:35:02 +0300

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,1 +1,20 @@
-
+#include "holberton.h"
+/**
+ * main - check the code for Holberton School students.
+ * @s: character
+ * @accept: characcter
+ * Return: Always 0.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+while(*s >= *accept)
+{
+int count = 0;
+   while (*accept != '\0') 
+   {
+      count++;
+      accept++;
+   }
+}
+   return count;
+}
