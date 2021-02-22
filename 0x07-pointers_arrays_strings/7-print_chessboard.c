ce32da98e45949b6b8cd2b6e6576baaad3b7adcc
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-22 19:39:48 +0300

    Update 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,11 +8,11 @@
 void print_chessboard(char (*a)[8])
 {
 int i, j;
-for (i = 0; i <= 8; i++)
+for (i = 0; i < 8; i++)
 {
 for (j = 0; a[j] != '\0'; j++)
 {
-_putchar('0' + a[j]);
+_putchar(a[j] + '\0');
 }
 }
 }
