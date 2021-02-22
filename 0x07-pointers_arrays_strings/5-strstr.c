276dbb31579db2599b97defcfc319e3807d1d06c
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-22 18:50:55 +0300

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,1 +1,24 @@
-
+#include "holberton.h"
+/**
+ * _strstr - check the code for Holberton School students.
+ * @haystack: SRC
+ * @needle: substr
+ * Return: Always 0.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+while (*haystack)
+{
+char *Begin = haystack;
+char *pattern = needle;
+while (*haystack && *pattern && *haystack == *pattern)
+{
+haystack++;
+ pattern++;
+}
+if (!*pattern)
+return Begin;
+haystack = Begin + 1;
+}
+return NULL;
+}
