44a4c40ccc58903a1224f5bdb0bb6c1e98f20d3a
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-22 20:12:37 +0300

    Update 8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,1 +1,20 @@
-
+#include "holberton.h"
+/**
+*print_diagsums - print character in a string
+*
+*@a: print char
+*size: sizes
+*Return: number of characters
+*/
+void print_diagsums(int *a, int size);
+{
+int i, j;
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+_putchar(*(a[i] + j));
+}
+_putchar('\n');
+}
+}
