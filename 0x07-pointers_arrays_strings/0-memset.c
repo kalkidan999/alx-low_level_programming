7afb04c93b1ab9bac0844a62a2c59af1db06bde2
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-22 07:02:31 +0300

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,8 +10,7 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 unsigned int index;
-unsigned char *memory = s, value = b;
 for (index = 0; index < n; index++)
-memory[index] = value;
-return (memory);
+s[index] = b;
+return (s);
 }
