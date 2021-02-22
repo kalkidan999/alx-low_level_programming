04a89add06668df84fe2f59a7e36c9665e75e405
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-22 06:35:50 +0300

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,1 +1,19 @@
-
+#include "holberton.h"
+/**
+ * _memset - prints buffer 
+ * @s: the pointer of memory to print
+ * @b: the character of memory to print
+ * @n: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+n = 0;
+while (n >= 0)
+{
+**s = b;
+n++;
+}
+return (s);
+}
