19e409e49210d8b69cd76b909980b46eccaea970
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-22 22:36:14 +0300

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,14 +8,14 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-  unsigned int i;
-  unsigned int j;
-  for (i = 0; s[i] != '\0'; i++)
-    {
-      for (j = 0; accept[j] != s[i]; j++)
-	{
-	  if (accept[j] == '\0')
- return (i);
+unsigned int i;
+unsigned int j;
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; accept[j] != s[i]; j++)
+{
+if (accept[j] == '\0')
+return (i);
 }
 }
 return (i);
