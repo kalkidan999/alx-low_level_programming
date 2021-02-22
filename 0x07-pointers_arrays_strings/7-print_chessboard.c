738fb3d39e5ee3b10f682afd63a4847a3fd8fbcf
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-22 19:11:38 +0300

    Update 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,1 +1,20 @@
-
+#include "holberton.h"
+/**
+* print_chessboard -print character in a string
+*
+*@a: print char
+*Return: number of characters
+*/
+void print_chessboard(char (*a)[8])
+{
+unsigned int i;
+unsigned int j;
+for (i = 0; i <= 8; i++)
+{
+for (j = 0; a[j] != '\0'; j++)
+{
+return (a);
+}
+}
+return (0);
+}
