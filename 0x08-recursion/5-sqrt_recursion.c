33fdc2cf308514e8aa2fb18aaf027102af318015
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-24 07:32:11 -0800

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,1 +1,24 @@
-
+#include "holberton.h"
+/**
+ *_sqrt_recursion - check the code.
+ *@n: character
+ * Return: Always 0.
+ */
+int _sqrt_recursion(int n)
+{
+if (n == 0)
+{
+return (0);
+}
+if (n == 1)
+{
+return (1);
+}
+if (n == (n-1))
+{
+return (n * _sqrt_recursion(n - 1));
+}
+else
+{
+return (-1);
+}
