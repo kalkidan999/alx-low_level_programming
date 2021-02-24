10f52ee33c56c85f9118632af0428456bf8c6874
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-24 16:57:32 +0300

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,13 +1,13 @@
 #include "holberton.h"
 /**
- *_strlen_recursion - check the code for Holberton School students.
+ *_strlen_recursion - check the code.
  *@s: character
  * Return: Always 0.
  */
 int _strlen_recursion(char *s)
 {
-if (*s == '\0')  
-        return (0); 
-    else
-        return (1 + _strlen_recursion(s + 1)); 
+if (*s == '\0')
+return (0);
+else
+return (1 + _strlen_recursion(s + 1));
 }
