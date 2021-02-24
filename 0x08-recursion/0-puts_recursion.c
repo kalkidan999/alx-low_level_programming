c2fb2ab21fe8e255dab25dd8630c34fd5b803122
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-24 10:16:34 +0300

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,1 +1,14 @@
-
+#include "holberton.h"
+/**
+ * _puts_recursion - check the code for Holberton School students.
+ *@s: character
+ * Return: Always 0.
+ */
+void _puts_recursion(char *s)
+{
+if(*s)
+{
+_puts_recursion(s);
+_putchar(*s);
+}
+}
