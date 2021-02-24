da3c92030de2a60d71018c4adc8cdc7e77447aa7
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-24 17:03:45 +0300

    Update 3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,1 +1,21 @@
-
+#include "holberton.h"
+/**
+ *factorial - check the code.
+ *@n: character
+ * Return: Always 0.
+ */
+int factorial(int n)
+{
+if (n < 0)
+{
+return (-1);
+}
+else if ( n == 0)
+{
+return (0);
+}
+else
+{
+return (n * factorial(n + 1));
+}
+}
