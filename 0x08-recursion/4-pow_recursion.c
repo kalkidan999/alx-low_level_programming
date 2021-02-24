a9db337f70515c407c2744c27e01bc1f09bb37cf
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-24 07:16:16 -0800

    Update 4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,1 +1,22 @@
-
+#include "holberton.h"
+/**
+ *_pow_recursion - check the code.
+ *@x: character
+ *@y: character
+ * Return: Always 0.
+ */
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+{
+return (-1);
+}
+if (y == 0)
+{
+return (1);
+}
+if (y > 1)
+{
+pwr = x * _pow_recursion(x++);
+return (pwr);
+}
