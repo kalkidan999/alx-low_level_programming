f4630e2c2532103b66ce5349870250ac1a1310ac
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-24 07:06:06 -0800

    Update 3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -6,16 +6,13 @@
  */
 int factorial(int n)
 {
+int fact = 0;
 if (n < 0)
-{
 return (-1);
-}
-else if ( n == 0)
-{
+if (n == 0)
 return (0);
-}
-else
-{
-return (n * factorial(n + 1));
-}
+if (n == 1)
+return (1);
+fact = n * factorial(n - 1)
+return (fact);
 }
