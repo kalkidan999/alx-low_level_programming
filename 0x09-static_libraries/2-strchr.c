93c09d7167295bd4001a839322accd0e2b1992df
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-26 16:14:14 +0300

    Create 2-strchr.c

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+* _strchr - searches buffer
+*@s: memory to print
+*@c: memory to print
+* Return: Nothing.
+*/
+char *_strchr(char *s, char c)
+{
+while (*s != '\0')
+{
+if (*s == c)
+{
+return (s);
+}
+s++;
+}
+if (*s == c)
+{
+return (s);
+}
+return ('\0');
+return (0);
+}
