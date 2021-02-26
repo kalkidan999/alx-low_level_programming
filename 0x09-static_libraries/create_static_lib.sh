7ead2ba2ed50eb768704c9bf34d6298b0c09e2b9
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-26 15:00:44 +0300

    Update create_static_lib.sh

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,1 +1,8 @@
-
+#include "holberton.h"
+/**
+ *_print_rev_recursion - check the code for Holberton School students.
+ *@s: character
+ * Return: Always 0.
+ */
+gcc -c *.c
+ar -rc libholberton.a *.o
