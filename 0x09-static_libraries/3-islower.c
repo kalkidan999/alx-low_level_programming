4865633977ebc206cc4494c120b3301e5e9deace
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-26 15:51:02 +0300

    Create 3-islower.c

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _islower - check the code for Holberton School students.
+ * @c: c file
+ * Return: Always 0
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
