b32c1365eb1c1a947c592a32dda3c00224d3bc49
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-27 02:46:49 -0800

    Update 9-strcpy.c

--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -1,13 +1,20 @@
 #include "holberton.h"
-/**
- * _strncpy - prints buffer
- * @src: the pointer of memory to print
- * @dest: the character of memory to print
- * @n: the size of the memory to print
- *
- * Return: Nothing.
- */
-char *_strncpy(char *dest, char *src, int n)
-{
 
+/**
+ * *_strcpy - copy strg to another string
+ * @dest: pointer
+ * @src: poiter
+ * Return: numbers dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
 }
