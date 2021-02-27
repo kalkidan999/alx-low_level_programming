f59536c8f458089b393c4112961ace87b8c79eac
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-27 11:53:43 -0800

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,1 +1,20 @@
-
+#include <stdio.h>
+/**
+ * main - check the code for Holberton School students.
+ *@argc: character count
+ *@argv: array
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+int a , b;
+int count = a * b;
+if (argc > 0)
+{
+for (count = 0; count < argc; count++)
+{
+printf("%s\n", argv[count]);
+}
+}
+return (0);
+}
