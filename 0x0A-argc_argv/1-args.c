352d7fb6d0018898e9f827fb86fa586bb3cc3dcf
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-27 10:54:23 -0800

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,1 +1,13 @@
-
+#include <stdio.h>
+/**
+ * main - check the code for Holberton School students.
+ *@argc: character count
+ *@argv: array
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+int count = 0;
+printf("%d\n", argv[argc]);
+return (0);
+}
