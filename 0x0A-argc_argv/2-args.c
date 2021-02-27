515cf6ba2cf27c70115f5ae4342941f21b74c81f
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-27 11:42:20 -0800

    Update 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+int main (int argc, char *argv[])
+{
+  int count;
+
+  if (argc > 0)
+    {
+      for (count = 0; count < argc; count++)
+	{
+	  printf("argv[%d] = %s\n", count, argv[count]);
+	}
+  return 0;
+}
+}
