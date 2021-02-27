9c56d314f28c6f4d3c6cef93b2901433f49780db
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-27 10:27:37 -0800

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+/**
+ * main - check the code for Holberton School students.
+ *@argc: character count
+ *@argv: array
+ * Return: Always 0.
+ */
 int main(int argc, char *argv[])
 {
 printf("%s\n",argv[argc - 1]);
