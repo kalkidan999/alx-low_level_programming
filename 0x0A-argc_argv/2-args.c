3748ed24e3ec214c16cb06292f9cceb4837b194f
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-27 11:47:30 -0800

    Update 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
-
+/**
+ * main - check the code for Holberton School students.
+ *@argc: character count
+ *@argv: array
+ * Return: Always 0.
+ */
 int main (int argc, char *argv[])
 {
-  int count;
-
-  if (argc > 0)
-    {
-      for (count = 0; count < argc; count++)
-	{
-	  printf("%s\n", argv[count]);
-	}
-  }
-  return 0;
+int count;
+if (argc > 0)
+{
+for (count = 0; count < argc; count++)
+{
+printf("%s\n", argv[count]);
+}
+}
+return (0);
 }
