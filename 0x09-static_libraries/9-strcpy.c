91979674adc8f2d4d4f05687c5acf30d547836ee
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-27 02:06:39 -0800

    Create 9-strcpy.c

---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * _strncpy - prints buffer
+ * @src: the pointer of memory to print
+ * @dest: the character of memory to print
+ * @n: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+char *_strncpy(char *dest, char *src, int n);
+{
+
+}
