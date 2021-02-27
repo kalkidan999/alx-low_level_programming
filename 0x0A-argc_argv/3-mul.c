9552bc70b9f57c3a8a5a9d2e45864758b60f01ab
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-27 13:15:49 -0800

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,14 +10,14 @@ int main(int argc, char *argv[])
 {
 int i, mul = 1;
 if (argc != 3)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	for (i = 1; i < argc; i++)
-	{
-		mul *= atoi(argv[i]);
-	}
-	printf("%d\n", mul);
-	return (0);
+{
+printf("Error\n");
+return (1);
+}
+for (i = 1; i < argc; i++)
+{
+mul *= atoi(argv[i]);
+}
+printf("%d\n", mul);
+return (0);
 }
