b1795027c562598004501e12d952185204925dfe
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-27 01:58:57 -0800

    Create 6-abs.c

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "holberton.h"
+/**
+* _abs - does my stuff
+* @m: user input
+*
+* Return: absolute.
+*/
+int _abs(int m)
+{
+int result = (abs(m));
+return (result);
+}
