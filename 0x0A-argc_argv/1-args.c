fc4b5ba44931832da8ea39af3084fa6554d307a8
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-27 11:29:24 -0800

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,14 +5,14 @@
  *@argv: array
  * Return: Always 0.
  */
-int main (int argc, char *argv[])
+int main (int argc, char *argv[] __attribute__((unused)))
 {
   int count;
   if (argc >= 0)
     {
       for (count = 0; count < argc; count++)
 	{
-	  printf("%d %s\n", count, argv[count]);
+	  printf("%d %s\n", count);
 	}
   }
 
