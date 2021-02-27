0ac32c9884e4637ff351b8bc5f0cb8cbcb511961
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-27 13:22:49 -0800

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,1 +1,23 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - check the code for Holberton School students.
+ *@argc: character count
+ *@argv: array
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+int i, mul = 1;
+if (argc < 1)
+{
+printf("0\n");
+return (1);
+}
+for (i = 1; i < argc; i++)
+{
+mul *= atoi(argv[i]);
+}
+printf("%d\n", mul);
+return (0);
+}
