f1978e501bbb75b34430dd65ce211d2cf6199aae
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-27 11:08:02 -0800

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,20 +8,13 @@
 int main (int argc, char *argv[])
 {
   int count;
-
-  printf ("This program was called with \"%s\".\n",argv[0]);
-
-  if (argc > 1)
+  if (argc >= 0)
     {
       for (count = 1; count < argc; count++)
 	{
 	  printf("argv[%d] = %s\n", count, argv[count]);
 	}
-    }
-  else
-    {
-      printf("The command had no other arguments.\n");
-    }
+  }
 
   return 0;
 }
