6b1a028ad84d5b8f31838be76cb6ceae649d575c
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-27 22:48:03 -0800

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,12 +12,18 @@ int i, sum = 0;
 if (argc < 1)
 {
 printf("0\n");
-return (1);
 }
 for (i = 1; i < argc; i++)
 {
+if (isdigit(argv[i])
+    {
 sum += atoi(argv[i]);
+    }
+    else {
+      printf("Error\n");
+return (1);
 }
+    }
 printf("%d\n", sum);
 return (0);
 }
