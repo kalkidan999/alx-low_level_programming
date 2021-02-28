5e78b7083d3fcfe4572b8f385dae53208bb6241c
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-27 22:54:31 -0800

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 /**
  * main - check the code for Holberton School students.
  *@argc: character count
@@ -16,12 +17,15 @@ printf("0\n");
 for (i = 1; i < argc; i++)
 {
 if (isdigit(argv[i])
+    {
 sum += atoi(argv[i]);
+    }
     else 
+    {
       printf("Error\n");
 return (1);
 }
-    
+    }  
 printf("%d\n", sum);
 return (0);
 }
