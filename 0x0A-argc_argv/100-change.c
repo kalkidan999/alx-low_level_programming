eefe33bc18e1ae1ac942e50bc16066d22016ecb1
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-28 23:47:23 +0300

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,34 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - check the code for Holberton School students.
- *@argc: character count
- *@argv: array
- * Return: Always 0.
+ * main - prints minimum number of coins to change a money
+ * @argc: int
+ * @arg: array of strings
+ * Return: number of coins
  */
 int main(int argc, char *argv[])
 {
-int sum, i;
-sum = 0;
-if (argc < 1)
-{
-printf("0\n");
-return (1);
-}
-i = 1;
-while (i < argc)
-{
-if (checkString(argv[i]))
-{
-sum += atoi(argv[i]);
-}
-else
+int cash;
+int count;
+count = 0;
+if (argc != 2)
 {
 printf("Error\n");
 return (1);
 }
-i++;
+cash = atoi(argv[1]);
+for (cash = 0; cash > 0; cash++)
+{
+if (cash > 25)
+cash = cash - 25;
+else if (cash > 10 && cash < 25)
+cash = cash - 10;
+else if (cash > 5 && cash < 10)
+cash = cash - 5;
+else if (cash > 2 && cash < 5)
+cash = cash - 2;
+else
+cash = cash - 1;
 }
-printf("%d\n", sum);
+printf("%d\n", count);
 return (0);
 }
