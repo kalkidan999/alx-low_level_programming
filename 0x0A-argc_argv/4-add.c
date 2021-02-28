862fa821f3180ea01b4e77d53be03199f1894aaf
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-02-28 01:15:50 -0800

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,18 +3,16 @@
 
 int checkString(char *Str)
 {
-    char *ptr = Str;
-
-    while( *ptr )
-    {
-                if( ! (*ptr >= 0x30 && *ptr <= 0x39 ) )
-        {
-            return 0;
-        }
-        ptr++;
-    }
-
-    return 1;
+char *ptr = Str;
+while (*ptr )
+{
+if( ! (*ptr >= 0x30 && *ptr <= 0x39 ) )
+{
+return 0;
+}
+ptr++;
+}
+return 1;
 }
 /**
  * main - check the code for Holberton School students.
@@ -24,27 +22,27 @@ int checkString(char *Str)
  */
 int main(int argc, char *argv[])
 {
-int results, i;
-	results = 0;
-	if (argc < 3)
-	{
-		printf("0\n");
-		return (1);
-	}
-	i = 1;
-	while (i < argc)
-	{
-		if (checkString(argv[i]))
-		{
-			results += atoi(argv[i]);
-		}
-		else
-		{
-			printf("Error\n");
-			return (1);
-		}
-		i++;
-	}
-	printf("%d\n", results);
-	return (0);
+int sum, i;
+sum = 0;
+if (argc < 3)
+{
+printf("0\n");
+return (1);
+}
+i = 1;
+while (i < argc)
+{
+if (checkString(argv[i]))
+{
+sum += atoi(argv[i]);
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+i++;
+}
+printf("%d\n", sum);
+return (0);
 }
