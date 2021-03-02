b4b0f524660cb2cd89ba2e446f93eafdad79bb76
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-03-01 17:53:48 -0800

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,1 +1,27 @@
-
+#include "holberton.h"
+#include <stdlib.h>
+/**
+*alloc_grid - change value using
+*@width: manipulates width
+*@height: manipulates height
+*Return: succcess
+*/
+int **alloc_grid(int width, int height)
+{
+int *p;
+int i;
+width = 0;
+height = 0;
+if (width <= 0 || height <= 0)
+return (NULL);
+p = malloc(sizeof(int));
+if (p == NULL)
+{
+return (NULL);
+}
+for (int i=0; i < height; ++i)
+    {
+        p[i] = i*width;
+    }
+    return (p);
+}
