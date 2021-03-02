f03d39b19e2d1fdb7933e9fb9be30d93ecf6b7a2
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-03-01 17:06:47 -0800

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,1 +1,23 @@
-
+#include "holberton.h"
+#include <stdlib.h>
+/**
+*_strdup - change value using
+*@str: manipulates size
+*Return: memory
+*/
+char *_strdup(char *str)
+{
+int i;
+char *p;
+if (str == NULL)
+return (0);
+p = malloc(sizeof(char));
+if (p == '\0')
+return (0);
+int i;
+for (i = 0; p[i] != '\0'; i++)
+	{
+p[i] = str[i];
+	}
+return (p);
+}
