d3ff0f0e0bc36ecce311e7c2155d72b6fd300251
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-03-01 23:07:17 -0800

    Update 4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -8,23 +8,10 @@
 */
 void free_grid(int **grid, int height)
 {
-int width;
-int i;
-int h = height;
-if (width <= 0 || height <= 0)
-return (0);
-grid = malloc(height *sizeof(int *));
-if (!grid)
-return (0);
-while (height-- > 0)
+while (height-- >= 0)
 {
-grid[height] = malloc(width *sizeof(int));
-if (!grid[height])
-return (0);
+free(*(grid + height));
+height--;
 }
-for (height = 0; height < h; height++)
-for (i = 0; i < width; i++)
-grid[height][i] = 0;
-free(grid[height][i]);
-return (p);
+free(grid);
 }
