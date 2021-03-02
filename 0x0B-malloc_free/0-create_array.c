8f882169e86b191e97f12926d8002cf298a0fed3
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-03-01 16:53:03 -0800

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,10 +10,14 @@ char *create_array(unsigned int size, char c)
 {
 unsigned int i;
 int *p;
+if (size == 0)
+return (0);
 p = malloc((size) * sizeof(char));
+if (p == '\0')
+return (0);
 for (i = 0; i < size; i++)
 {
 p[i] = c;
 }
-return (p);
+return (*p);
 }
