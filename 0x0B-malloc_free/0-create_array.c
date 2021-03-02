a093532a652e7dc5a11eabcd78bd48fe8f5fdb19
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-03-01 16:46:46 -0800

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -6,12 +6,14 @@
 *@c: stores the input
 *Return: memory
 */
-char *create_array(int size, char c)
+char *create_array(unsigned int size, char c)
 {
 int i;
 int *p;
-p = malloc(size * sizeof(char));
+p = malloc((size) * sizeof(char));
 for (i = 0; i < size; i++)
-*p = c;
-_putchar(p[i]);
+{
+p[i] = c;
+}
+return (p);
 }
