1b077a5d413ac4b514f765914ce0de2a09f4bee6
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-03-01 16:27:42 -0800

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,1 +1,18 @@
-
+#include "holberton.h"
+#include <stdlib.h>
+/**
+*create_array - change value using
+*@size: manipulates size
+*@c: stores the input
+*Return: memory
+*/
+char *create_array(unsigned int size, char c)
+{
+int i;
+int *p;
+p = malloc(size * sizeof(char));
+for (i = 0; i < size; i++)
+{
+_putchar(p[i]);
+}
+}
