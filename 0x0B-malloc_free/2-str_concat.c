71f8f12f5b1ac4a2d789d5a41a20ae662cb97a6d
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-03-03 09:52:57 +0300

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,5 +1,24 @@
 #include "holberton.h"
 #include <stdlib.h>
+
+/**
+ * _strlen - length of a string
+ * @s: input char
+ * Return: length of a string
+ */
+
+int _strlen(char *s)
+{
+	int l = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		l++;
+	}
+	return (l);
+}
+
 /**
 *str_concat - change value using
 *@s1: manipulates char1
@@ -8,14 +27,19 @@
 */
 char *str_concat(char *s1, char *s2)
 {
+unsigned int a, b;
 char *p, *tmp;
-if (!s1 || !s2)
-return (NULL);
-p = malloc(sizeof(char));
-if (p == NULL)
-{
-return (NULL);
-}
+if (!s1)
+s1 = "";
+else
+a = _strlen(s1);
+if (!s2)
+s2 = "";
+else
+b = _strlen(s2);
+p = malloc(a + b + 1);
+if (!p)
+return (0);
 tmp = p;
 while (*s1)
 *tmp++ = *s1++;
