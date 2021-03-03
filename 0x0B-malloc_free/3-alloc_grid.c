68ddc2cab41efadde136f5eb36f362aa2fb9fae6
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-03-03 10:54:43 +0300

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -20,13 +20,8 @@ while (height-- > 0)
 {
 p[height] = malloc(width * sizeof(int));
 if (!p[height])
-while (height >= 0)
-{
 free(p[height]);
-height--;
-}
 free(p);
-return (0);
 }
 for (height = 0; height < h; height++)
 for (i = 0; i < width; i++)
