704f023aef4f62c437d08ecf34bda4b96e36f19b
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-03-04 16:24:24 +0300

    Update 3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,1 +1,23 @@
+#include <stdlib.h>
+#include "holberton.h"
 
+/**
+ * array_range - allocate a 2d integer grid
+ *
+ * @min: minimum of grid
+ * @max: maximum of grid
+ * Return: returns pointer to grid created
+ */
+int *array_range(int min, int max)
+{
+int *p, i;
+if (min > max)
+return (NULL);
+p = malloc(((max - min) + 1) * sizeof(int));
+if (p == NULL)
+return (NULL);
+i = 0;
+while (min <= max)
+p[i++] = min++;
+return (p);
+}
