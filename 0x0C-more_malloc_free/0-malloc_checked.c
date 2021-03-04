1ca9e3d1f7bc7a2b0d99eb83fd6cde719a46779b
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-03-04 10:37:57 +0300

    Update 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,1 +1,25 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+* malloc_checked - crete memory allocation
+* @b: int
+*Return: pointer
+*/
+void *malloc_checked(unsigned int b)
+{
+int *p;
+unsigned int i;
+if (b <= 0)
+{
+return (NULL);
+}
+p = malloc((b) * sizeof(char));
+if (p == NULL)
+return (NULL);
+for (i = 0; i < b; i++)
+{
+p[i] = b;
+}
+return (p);
+}
