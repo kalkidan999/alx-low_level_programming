786e289a663d711f6c62f7c26944c1192473810f
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-03-04 16:04:28 +0300

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,1 +1,21 @@
+#include <stdlib.h>
+#include "holberton.h"
 
+/**
+ * _calloc - allocate a 2d integer grid
+ *
+ * @nmemb: block of memory
+ * @size: size of grid
+ * Return: returns pointer to grid created
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+unsigned int i;
+char *p;
+if (nmemb == 0 || size == 0)
+return (NULL);
+p = malloc(nmemb * size);
+if (p == NULL)
+return (NULL);
+for (i = 0; i != size; i++)
+*(p + (size * i)) = 0;
+return (p);
