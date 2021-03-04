921a165fbbdcb4f47658697117f5e1dc156688e5
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-03-04 11:11:48 +0300

    Update 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -9,13 +9,8 @@
 void *malloc_checked(unsigned int b)
 {
 void *p;
-unsigned int i;
-p = malloc(sizeof(*p) * b);
+p = malloc(b);
 if (p == NULL)
 exit(98);
-for (i = 0; i < b ; i++)
-{
-p[i] = b;
-}
 return (p);
 }
