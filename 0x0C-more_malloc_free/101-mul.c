334bd0cc988e253b0e7b3d094ce99f5a14aab701
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-03-04 17:02:24 +0300

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -8,19 +8,19 @@
  */
 int _atoi(char *s)
 {
-	int retval, i, neg;
-	i = 0;
-	retval = 0;
-	neg = -1;
-	while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
-	{
-	if (s[i] == '-')
-	neg *= -1;
-	i++;
-	}
-	while (s[i] != '\0' && (s[i] >= '0' && s[i] <= '9'))
-	retval = (retval * 10) - (s[i++] - '0');
-	return (retval * neg);
+int retval, i, neg;
+i = 0;
+retval = 0;
+neg = -1;
+while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
+{
+if (s[i] == '-')
+neg *= -1;
+i++;
+}
+while (s[i] != '\0' && (s[i] >= '0' && s[i] <= '9'))
+retval = (retval * 10) - (s[i++] - '0');
+return (retval * neg);
 }
 /**
  * main - multiplies two positive numbers
@@ -30,7 +30,7 @@ int _atoi(char *s)
  */
 int main(int argc, char *argv[])
 {
-int a,b, i, j;
+int a, b, i, j;
 unsigned long mul;
 if (argc != 3)
 {
@@ -50,7 +50,7 @@ exit(98);
 }
 a = _atoi(argv[1]);
 b = _atoi(argv[2]);
-mul = a * b;
+mul = a *b;
 printf("%lu\n", mul);
 return (0);
 }
