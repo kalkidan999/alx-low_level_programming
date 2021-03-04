7a19f6886dfc966bd4838b119989dbf02d3070fa
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-03-04 11:45:25 +0300

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -38,13 +38,13 @@ if (!s2)
 s2 = "";
 else
 b = _strlen(s2);
-p = malloc(a + b - n);
+p = malloc(a + b + 1);
 if (!p)
 return (0);
 tmp = p;
 while (*s1)
 *tmp++ = *s1++;
-while ((*tmp++ = *s2++))
+while ((*tmp++ = *s2 + n))
 ;
 return (p);
 }
