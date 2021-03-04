a1e47f36e19ed7228786b50d30716301c2006fc6
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-03-04 11:31:12 +0300

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,1 +1,50 @@
+#include "holberton.h"
+#include <stdlib.h>
 
+/**
+ * _strlen - length of a string
+ * @s: input char
+ * Return: length of a string
+ */
+
+int _strlen(char *s)
+{
+	int l = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		l++;
+	}
+	return (l);
+}
+
+/**
+*string_nconcat - change value using
+*@s1: manipulates char1
+*@s2: manipulates char2
+*@n: manipulates integer
+*Return: memory
+*/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+unsigned int a, n;
+char *p, *tmp;
+if (!s1)
+s1 = "";
+else
+a = _strlen(s1);
+if (!s2)
+s2 = "";
+else
+n = _strlen(s2);
+p = malloc(a + n + 1);
+if (!p)
+return (0);
+tmp = p;
+while (*s1)
+*tmp++ = *s1++;
+while ((*tmp++ = *s2++))
+;
+return (p);
+}
