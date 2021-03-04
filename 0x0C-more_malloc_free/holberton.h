352196e7dc257e5beacd44ca693eb3257f4a57a0
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-03-04 22:51:50 +0300

    Update holberton.h

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -1,7 +1,6 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 int _putchar(char c);
-int main(int argc, char *argv[]);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
