250f8915e71ac8081160929d5fb1a102a231fc48
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-03-08 09:24:30 -0800

    Update 5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,1 +1,15 @@
-
+#include "dog.h"
+/**
+ * free_dog - check the code for Holberton School students.
+ * @d: decimal
+ * Return: Always 0.
+ */
+void free_dog(dog_t *d)
+{
+if (d == NULL)
+{
+return;
+free(d->name);
+free(d->owner);
+free(d);
+}
