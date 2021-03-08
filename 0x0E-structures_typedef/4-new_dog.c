80d80d1411e1eb34501a30e7c068bf7eb5cf0b1d
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-03-08 15:46:16 +0300

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,1 +1,29 @@
-
+#include "dog.h"
+#include <stdio.h>
+/**
+ * print_dog - check the code for Holberton School students.
+ * @d: pointer
+ * Return: Always 0.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+if (d)
+{
+if (d->name != NULL)
+{
+printf("Name: %s\n", d->name);
+}
+else
+{
+printf("Name: (nil)\n");
+}
+printf("Age: %f\n", d->age);
+if (d->owner)
+{
+printf("Owner: %s\n", d->owner);
+}
+else
+{
+printf("Owner: (nil)\n");
+}
+}
+}
