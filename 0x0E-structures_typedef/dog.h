7fa14ad890a8945fe0442f9f2ac93cb67aa82586
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-03-08 14:57:46 +0300

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,1 +1,17 @@
+#ifndef FILE_D
+#define FILE_D
 
+/**
+ * struct dog - structure dog
+ * @name: atribute pointer to char name's dog
+ * @age: atribute float age's dog
+ * @owner: atribute pointer to char owner's dog
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
