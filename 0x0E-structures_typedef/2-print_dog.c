9d940b546ed3a52b85394bd696cd4c0c1ba89dd6
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-03-08 15:29:27 +0300

    Update 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,1 +1,19 @@
-
+#include "dog.h"
+#include <studio.h>
+/**
+ * print_dog - check the code for Holberton School students.
+ * @d: pointer
+ * Return: Always 0.
+ */
+void print_dog(struct dog *d)
+{
+struct dog;
+if (!d)
+{
+printf("nil\n");
+}
+else
+{
+*d = &dog;
+}
+}
