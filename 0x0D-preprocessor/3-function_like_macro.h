dc33ce4090268eb4756e39a657d687d5cba30855
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-03-08 11:39:01 +0300

    Update 3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,9 @@
-#ifndef ABS_H
-#define ABS_H
-#define ABS(x) ((x<0)?(-x):(x))
-#endif
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int ABS(int x)
+{
+return ((x<0)?(-x):(x));
+}
