0ee494b1c29d3f94d3f3f9202d1fadc3c1bb6877
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-03-08 16:01:14 +0300

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -7,26 +7,37 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+int i = 0, j = 0, k;
 dog_t *d;
-if (d)
+while (name[i] != '\0')
+i++;
+while (owner[j] != '\0')
+j++;
+d = malloc(sizeof(dog_t));
+if (d == NULL)
 {
-if (d->name != NULL)
+free(d);
+return (NULL);
+}
+d->name = malloc(i * sizeof(d->name));
+if (d->name == NULL)
 {
-printf("Name: %s\n", d->name);
+free(d->name);
+free(d);
+return (NULL);
 }
-else
+for (k = 0; k <= i; k++)
+d->name[k] = name[k];
+d->age = age;
+d->owner = malloc(j * sizeof(d->owner));
+if (d->owner == NULL)
 {
-printf("Name: (nil)\n");
+free(d->owner);
+free(d->name);
+free(d);
+return (NULL);
 }
-printf("Age: %f\n", d->age);
-if (d->owner)
-{
-printf("Owner: %s\n", d->owner);
-}
-else
-{
-printf("Owner: (nil)\n");
-}
-}
+for (k = 0; k <= j; k++)
+d->owner[k] = owner[k];
 return (d);
 }
