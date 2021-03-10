1edc26843a8cdbb7521678dc1b80f38fa85dd255
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-03-10 11:15:49 +0300

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,1 +1,13 @@
-
+#include "function_pointers.h"
+/**
+ * print_name - prints a name as is
+ * @name: name of the person
+ * @f: void
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+if( name && f)
+{
+f(name);
+}
