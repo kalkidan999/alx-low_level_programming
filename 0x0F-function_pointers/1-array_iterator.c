42dae4c86189458569d5ed583d9529b168272d2b
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-03-10 14:44:10 +0300

    Update 1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,1 +1,19 @@
-a
+#include "function_pointers.h"
+/**
+ * array_iterator - prints a name as is
+ * @array: name of the person
+ * @size: void
+ * @action: void
+ * Return: Nothing.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+unsigned int i;
+if (array && size && action)
+{
+for (i = 0; i < size; i++)
+{
+(*action)(array[i]);
+}
+}
+}
