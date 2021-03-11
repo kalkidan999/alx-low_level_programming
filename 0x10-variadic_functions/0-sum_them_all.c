33f0e06b06a95e8831785f0bfce4192bf4a4187b
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-03-11 10:44:26 +0300

    Update 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,1 +1,23 @@
-
+#include "variadic_functions.h"
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+unsigned int i;
+int sum = 0;
+va_list sum;
+va_start(sum, n);
+if (n == 0)
+{
+return 0;
+}
+for (i = 0; i < n; i++)
+{
+sum += va_arg(sum, int);
+}
+va_end (sum);
+return (sum);
+}
