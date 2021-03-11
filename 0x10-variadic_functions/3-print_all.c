4de0fe9ea355e318fdaa2818a60291b155c90614
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-03-11 16:05:32 +0300

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,1 +1,40 @@
-
+#include "variadic_functions.h"
+#include <stdio.h>
+/**
+ * print_all - print character.
+ * Return: Always 0.
+ */
+void print_all(const char * const format, ...)
+{
+char c;
+int i = 0;
+float f;
+char *s;
+va_list arguments;
+va_start(arguments, format);
+ while (*format != '\0')
+ {
+ if (*format == 'd')
+ {
+ int i = va_arg(args, int);
+ printf("%d\n", i);
+ }
+if (*format == 'c')
+{
+int c = va_arg(args, int);
+printf("%c\n", c);
+}
+if (*format == 'f')
+{
+double d = va_arg(args, double);
+printf("%f\n", d);
+}
+if (*format == 's')
+{
+int s = va_arg(args, int);
+printf("%s\n", s);
+ }
+ ++format;
+ }
+va_end(args);
+}
