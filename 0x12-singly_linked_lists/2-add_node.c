34b29fcb5835fae42746659c4650112684b14cfa
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-03-18 14:28:57 +0300

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,7 +9,7 @@ list_t *add_node(list_t **head, const char *str)
 {
 head = NULL;
 struct list_t* newnode=(struct list_t*)malloc(sizeof(struct list_t*));
-char *target = strdup(str);
+char *target = strdup(*str);
 newnode->len = target;
 newnode->next = head;
 head = newnode;
