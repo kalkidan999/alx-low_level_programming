8a4053c18894e34dd9a5c938f60a11378b004030
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-03-18 10:33:34 +0300

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,1 +1,24 @@
-
+#include "holberton.h"
+/**
+* create_array - multiplies two numbers
+* @size: int
+* @c: array of strings
+*Return: pointer
+*/
+size_t print_list(const list_t *h)
+{
+struct node {
+int data;
+h next;
+};
+typedef struct node node;
+h first = NULL;
+first = malloc(sizeof(node));
+first->next = NULL;
+first->data = list_t;
+h temp = malloc(sizeof(node);
+temp->next = first;
+first = temp;
+printf("%s", first->data);
+return (0);
+}
