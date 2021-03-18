f0d3348b35dd222b76fb5ec378005da7a5226180
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-03-18 20:27:05 +0300

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
 void main(void) __attribute__ ((constructor));
-
 /**
- * main - prints a sentence before the main
+ * stat - prints a sentence before the main
  * function is executed
  */
-void main(void)
+void stat(void)
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
 }
