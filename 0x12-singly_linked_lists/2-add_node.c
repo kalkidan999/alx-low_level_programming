da7a391237d20a9c7536b3df083c410aa1d9c241
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-03-18 14:03:57 +0300

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,1 +1,16 @@
-
+#include "lists.h"
+/**
+* list_len - prints list
+* @h: pointer
+*Return: pointer
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+int i = 0;
+while (h)
+{
+h = h->next;
+i++;
+}
+return (i);
+}
