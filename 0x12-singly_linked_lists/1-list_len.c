15a141a4de943e7d57eaca4e9f2c127a62436c98
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-03-18 11:53:31 +0300

    Update 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,1 +1,17 @@
-a
+#include "lists.h"
+/**
+* print_list - prints list
+* @list_t: struct
+* @h: pointer
+*Return: pointer
+*/
+size_t print_list(const list_t *h)
+{
+size_t i = 0;
+while (h)
+{
+h = h->next;
+i++;
+}
+return (i);
+}
