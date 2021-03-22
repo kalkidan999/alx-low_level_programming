b8236496346bb4b1dd32aa374cd47024fe61403f
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-03-22 11:15:01 -0700

    Update 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,1 +1,22 @@
-
+#include "lists.h"
+/**
+* get_nodeint_at_index - free list
+* @head: pointer
+* @index: to find nth character
+*Return: pointer
+*/
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+listint_t *temp = head;
+int count = 0;
+if (head == NULL)
+{
+return (NULL);
+}
+while (temp != NULL)
+if (count == index)
+return (temp->n); 
+count++;
+temp = temp->next;
+return(temp);
+}
