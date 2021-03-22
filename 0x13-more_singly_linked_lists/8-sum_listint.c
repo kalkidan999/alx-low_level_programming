51655149f3a4c57562576508341fa54b3b6c2e9c
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-03-22 12:09:40 -0700

    Update 8-sum_listint.c

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -7,14 +7,14 @@
 int sum_listint(listint_t *head)
 {
 int sum = 0;
-while (head)
-{
-sum += head->n;
-head = head->next;
 if (head == NULL)
 {
 return (0);
 }
+while (head)
+{
+sum += head->n;
+head = head->next;
 }
 return (sum);
 }
