4c70bff73ee6c5955507c5e27e1013494ad6c3da
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-03-22 12:22:17 -0700

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,1 +1,22 @@
-
+#include "lists.h"
+/**
+* insert_nodeint_at_index - insert node at nth index
+* @head: pointer
+* @idx: nth index
+* @n: value
+*Return: pointer
+*/
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+unsigned int i = 0;
+	listint_t *temp = *head;
+	while (temp && i < idx)
+	{
+		temp = temp->next;
+		i++;
+	}
+temp->n = n;
+temp->next = *head
+*head = temp;
+return (temp);
+}
