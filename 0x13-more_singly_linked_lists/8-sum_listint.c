b22383be1f8cf8dc05080a8138f3cd657bb4c389
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-03-22 11:59:44 -0700

    Update 8-sum_listint.c

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,1 +1,16 @@
-
+#include "lists.h"
+/**
+* sum_listint - free list
+* @head: pointer
+*Return: pointer
+*/
+int sum_listint(listint_t *head)
+{
+int sum = 0;
+while (head)
+{
+sum += head->n;
+head = head->next;
+}
+return (sum);
+}
