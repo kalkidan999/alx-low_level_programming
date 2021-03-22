b7278483fde17f47c8e96559e25cb0e7e5a177b7
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-03-22 13:20:17 -0700

    Update 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,1 +1,23 @@
-
+#include "lists.h"
+/**
+* delete_nodeint_at_index - insert node at nth index
+* @head: pointer
+* @index: nth index
+*Return: pointer
+*/
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+unsigned int i = 0;
+listint_t *newnode;
+newnode = malloc(sizeof(listint_t));
+newnode->n = n;
+newnode->next = NULL;
+}
+if (index == 0)
+{
+newnode->next = *head;
+*head = newnode;
+free(*head);
+return (*head);
+}
+}
