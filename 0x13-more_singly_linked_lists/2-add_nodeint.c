9f52abb2d1549aa5f4999ec15050a5edf582e30c
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-03-22 15:51:46 +0300

    Update 2-add_nodeint.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,1 +1,13 @@
-
+#include "lists.h"
+/**
+* list_len - prints list
+* @h: pointer
+*Return: pointer
+*/
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+n->val = val;
+n->next = *head;
+*head = n;
+return (n);
+}
