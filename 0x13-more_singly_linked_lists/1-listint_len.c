c396c04f66c7943be3262b78cb127b90525a8942
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-03-22 14:18:59 +0300

    Update 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,1 +1,17 @@
-
+#include "lists.h"
+/**
+* listint_len - prints n count n list
+* @h: pointer to first
+*Return: pointer
+*/
+size_t listint_len(const listint_t *h)
+{
+size_t i = 0;
+while (h)
+{
+printf("%i\n", h->n);
+h = h->next;
+i++;
+}
+return (h);
+}
