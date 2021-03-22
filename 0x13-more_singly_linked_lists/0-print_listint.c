48df15c4e368c3c26bbac29b260b8a0d57413095
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-03-22 11:13:15 +0300

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,1 +1,17 @@
-
+#include "lists.h"
+/**
+* print_listint - prints n count n list
+* @h: pointer to first
+*Return: pointer
+*/
+size_t print_listint(const listint_t *h)
+{
+size_t i = 0;
+while (h)
+{
+printf("%u %s\n", h->len, h->str);
+h-> next;
+i++;
+}
+return (i);
+}
