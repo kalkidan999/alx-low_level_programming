bccc137795ad269714cbcc5e8c0129925a4aa07f
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-03-22 11:37:13 -0700

    Update 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,10 +9,6 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 listint_t *temp = head;
 unsigned int count = 0;
-if (head == NULL)
-{
-return (NULL);
-}
 while (temp != NULL && count < index)
 temp = temp->next;
 count++;
