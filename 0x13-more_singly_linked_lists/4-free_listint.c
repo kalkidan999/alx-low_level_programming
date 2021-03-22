fe1218af181d33f804eba3c7ae8832b7bbd9f574
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-03-22 09:51:37 -0700

    Update 4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,1 +1,16 @@
-
+#include "lists.h"
+/**
+* free_listint - free list
+* @head: pointer
+*Return: pointer
+*/
+void free_listint(listint_t *head)
+{
+listint_t *temp;
+if (head != NULL)
+{
+temp = head->next;
+free(head);
+head = temp;
+}
+}
