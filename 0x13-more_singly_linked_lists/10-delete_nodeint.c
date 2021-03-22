4e08635ff8c0e29c43a526c0d04c850924f2a650
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-03-22 13:36:07 -0700

    Update 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -7,11 +7,26 @@
 */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-listint_t *newnode;
-newnode = malloc(sizeof(listint_t));
+listint_t *temp = *head;
+listint_t *current = NULL;
+unsigned int i = 0;
+if (*head == NULL)
+return (-1);
 if (index == 0)
-newnode->next = *head;
-(*head)->next = newnode->next;
-free(newnode);
+{
+*head = (*head)->next;
+free(temp);
+return (1);
+}
+while (i < index - 1)
+{
+if (!temp || !(temp->next))
+return (-1);
+temp = temp->next;
+i++;
+}
+current = temp->next;
+temp->next = current->next;
+free(current);
 return (1);
 }
