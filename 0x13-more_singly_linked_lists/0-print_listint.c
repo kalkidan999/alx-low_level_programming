8ed5bd8282710d8a55935c1096babce1d663149d
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-03-23 20:45:40 +0300

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,14 +6,14 @@
 */
 size_t print_listint(const listint_t *h)
 {
-size_t i;
+int i;
 if (h == NULL)
 {
 return (0);
 }
 for (i = 0; h != NULL; i++)
 {
-printf("%i\n", (*h).n);
+printf("%d\n", (*h).n);
 h = (*h).next;
 }
 return (i);
