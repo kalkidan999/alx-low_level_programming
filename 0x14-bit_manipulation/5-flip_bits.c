c4860aabb82594ff6f6a54a734dea1421ec3d9dd
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-03-25 22:16:47 +0300

    Update 5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -11,7 +11,7 @@ int count = 0;
 while (n > 0)
 {
 count++;
-n &= (n-1);
+n &= (n - 1);
 }
 return (count);
 }
@@ -23,5 +23,5 @@ return (count);
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-return countSetBits(n ^ m);
+return (countSetBits(n ^ m));
 }
