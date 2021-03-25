89dae61ab5a646b5c457f734dbc23f92c72e45a2
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-03-25 21:39:27 +0300

    Update 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,8 +7,8 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
+unsigned long int status = (n >> index) & 1;
 if (index > 32)
 return (-1);
-unsigned long int status = (n >> index) & 1;
 return (status);
 }
