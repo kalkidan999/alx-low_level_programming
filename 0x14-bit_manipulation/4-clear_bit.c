129ec49630c3fd9e38475bdd28d5d4ec20742972
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-03-25 21:59:53 +0300

    Update 4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,1 +1,15 @@
-
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * clear_bit - sets index to 0 
+ * @n: containing the decimal number
+ * @index: containing the index
+ * Return: the converted number
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+if (index > sizeof(n) * 8)
+return (-1);
+*n |= (1 << index);
+return (0);
+}
