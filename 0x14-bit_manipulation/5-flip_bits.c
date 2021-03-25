49d5f0953a494b034afa72bb62e50ceed665a02d
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-03-25 22:14:03 +0300

    Update 5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,28 +1,26 @@
 #include "holberton.h"
 #include <stdlib.h>
 /**
+ * countSetBits - counter
+ * @n: containing the number
+ * Return: the converted number
+ */
+int countSetBits(int n)
+{
+int count = 0;
+while (n > 0)
+{
+count++;
+n &= (n-1);
+}
+return (count);
+}
+/**
  * flip_bits - flips index to x0r
  * @n: containing the number
  * @m: containing the number
  * Return: the converted number
  */
-int countSetBits(int n)
-{
-
-    int count = 0;
-
-    while (n > 0)
-
-    {
-
-        count++;
-
-        n &= (n-1);
-
-    }
-
-    return count;
-}
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 return countSetBits(n ^ m);
