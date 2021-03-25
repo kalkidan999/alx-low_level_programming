275b87d44e0f5e6d7f202cdf003f48fd93e4a203
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-03-25 21:47:07 +0300

    Update 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,1 +1,14 @@
-
+#include "holberton.h"
+/**
+ * set_bit - gets int to index number
+ * @n: containing the decimal number
+ * @index: containing the index
+ * Return: the converted number
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+unsigned long int status = (1 << index) | n;
+if (index > 32)
+return (-1);
+return (status);
+}
