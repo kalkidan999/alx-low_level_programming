8a5d94e344b35583dbfd6a9af781d5bbaae6e77e
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-03-25 21:49:22 +0300

    Update 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -7,7 +7,7 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-unsigned long int status = (1 << index) | n;
+unsigned long int status = (1 << index) | *n;
 if (index > 32)
 return (-1);
 return (status);
