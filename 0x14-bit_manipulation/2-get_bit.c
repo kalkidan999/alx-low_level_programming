34d11fe8982e6db3e90bea78043df1040ba7f400
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-03-25 21:32:50 +0300

    Update 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,1 +1,12 @@
-
+#include "holberton.h"
+/**
+ * get_bit - gets int to index number
+ * @n: containing the decimal number
+ * @index: containing the index
+ * Return: the converted number
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+unsigned long int status = (n >> index) & 1;
+return (status);
+}
