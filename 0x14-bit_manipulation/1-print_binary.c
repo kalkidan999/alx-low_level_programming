13f1d5dc04007a1726a25aa1df503b3f64afd282
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-03-25 21:18:34 +0300

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,1 +1,24 @@
-
+#include "holberton.h"
+/**
+ * print_binary - converts int to binary number
+ * @n: containing the decimal number
+ * Return: the converted number
+ */
+void print_binary(unsigned long int n)
+{
+int i, count = 0;
+unsigned long int pnt;
+for (i = 31; i >= 0; i--)
+{
+pnt = n >> i;
+if (pnt & 1)
+{
+_putchar('1');
+count++;
+}
+else if (pnt)
+_putchar('0');
+}
+if (!pnt)
+_putchar('0');
+}
