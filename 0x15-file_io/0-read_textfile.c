30f8744387c6b5e34fdba85f434150725448dd30
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-03-29 14:52:05 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,1 +1,26 @@
-
+#include "holberton.h"
+#include <unistd.h> 
+#include <fcntl.h>
+#include <stdlib.h>
+/**
+ * read_textfile - converts a binary number to unsigned int
+ * @filename: pointer to string
+ * @letters: string containing the binary number
+ * Return: the converted number
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+int count, fd;
+char buf[1024];
+fd = open(filename, 0_RDWR);
+if (filename == -1 || filename == NULL)
+{
+return (0);
+}
+while (letter != '\0')
+{
+count++;
+letter++;
+}
+read(fd, buf , count);
+}
