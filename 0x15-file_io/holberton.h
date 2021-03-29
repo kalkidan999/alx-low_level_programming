e857f8949d5cdf8a09d590ca84cf738ac0a41010
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-03-29 15:05:19 +0300

    Update holberton.h

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -1,6 +1,6 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
-size_t read_textfile(const char *filename, size_t letters);
+ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 #endif
