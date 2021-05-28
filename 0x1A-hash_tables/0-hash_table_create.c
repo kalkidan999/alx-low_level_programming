0e0987d7bde831511894d03e83b15ddd07f110ce
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-05-28 20:44:35 +0300

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,9 +1,8 @@
 #include "hash_tables.h"
-
 /**
  * hash_table_create - create a hash table
- *
  * @size: Table size
+ * return: success
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
