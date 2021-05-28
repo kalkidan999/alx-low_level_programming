32d153d293f2feca10d9091ac49cd3393afeaa52
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-05-28 20:51:50 +0300

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,14 +9,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 hash_table_t *table;
 table = malloc(sizeof(hash_table_t));
 if (table == NULL)
-{
 return (NULL);
-}
 table->size = size;
 table->array = malloc(sizeof(table->array) * size);
 if (table->array == NULL)
-{
 return (NULL);
-}
 return (table);
 }
