15b60238fc67bc9c8c6df50f45298b13c26bf8a2
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-05-28 20:34:27 +0300

    Create 4-hash_table_get.c

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves a value
+ * @ht: hash table
+ * @key: key
+ * Return: Value with key
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+unsigned long int index;
+hash_node_t *node;
+if (!ht || !key)
+return (NULL);
+index = key_index((const unsigned char *)key, ht->size);
+node = ht->array[index];
+while (node)
+{
+if (strcmp(node->key, key) == 0)
+return (node->value);
+node = node->next;
+}
+return (NULL);
+}
