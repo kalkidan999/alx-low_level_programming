8dd18db7d0b176423c2c9b8119c4710e9de75789
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-09-02 10:51:42 +0300

    fix error

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -10,4 +10,5 @@
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
-int inter
+int interpolation_search(int *array, size_t size, int value);
+#endif
