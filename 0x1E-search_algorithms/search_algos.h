65246f65e978456994e0254927b2359869c20258
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-09-02 11:23:46 +0300

    Update search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,7 +5,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 /** Prototype */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
